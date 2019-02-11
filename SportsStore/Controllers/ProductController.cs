bf3c85e60e4a7367ec3b43aa5c1bfe5b89015f19
Abdullah Hashim <roxas0zero@gmail.com>
2019-02-12 01:16:01 +0300

    added first controller

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public IActionResult List()
+        {
+            return View(_repository.Products);
+        }
+    }
+}
---
+++ SportsStore/Models/FakeProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Models
+{
+    public class FakeProductRepository : IProductRepository
+    {
+        public IQueryable<Product> Products => new List<Product>
+        {
+            new Product { Name = "Football", Price = 25},
+            new Product { Name = "Surf Board", Price = 40},
+            new Product { Name = "Running Shoes", Price = 65}
+        }.AsQueryable<Product>();
+    }
+}
